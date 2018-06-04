071ba1e60cf8178a3fb112be5e33338e5d162c2d
slimsh25 <fahadshadihassan@gmail.com>
2018-06-04 10:03:11 +0300

    2 commit

---
+++ SportsStore.UnitTests/AdminSecurityTests.cs
@@ -0,0 +1,58 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportsStore.WebUI.Controllers;
+using SportsStore.WebUI.Infrastructure.Abstract;
+using SportsStore.WebUI.Models;
+
+namespace SportsStore.UnitTests
+{
+    /// <summary>
+    /// Summary description for AdminSecurityTests
+    /// </summary>
+    [TestClass]
+    public class AdminSecurityTests
+    {
+        [TestMethod]
+        public void Can_Login_With_Valid_Credentials()
+        {
+            // Arrange - create a mock authentication provider
+            Mock<IAuthProvider> mock = new Mock<IAuthProvider>();
+            mock.Setup(m => m.Authenticate("admin", "admin")).Returns(true);
+            // Arrange - create the view model
+            LoginViewModel model = new LoginViewModel
+            {
+                UserName = "admin",
+                Password = "admin"
+            };
+            // Arrange - create the controller
+            AccountController target = new AccountController(mock.Object);
+            // Act - authenticate using valid credentials
+            ActionResult result = target.Login(model, "/MyURL");
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(RedirectResult));
+            Assert.AreEqual("/MyURL", ((RedirectResult)result).Url);
+        }
+
+        [TestMethod]
+        public void Cannot_Login_with_Invalid_Credentials() 
+        {
+            //Arrange- create a mock authentication provider
+            Mock<IAuthProvider> mock = new Mock<IAuthProvider>();
+            mock.Setup(m => m.Authenticate("baduser", "badpass")).Returns(false);
+
+            //Arrange -create the view model
+            LoginViewModel model = new LoginViewModel { UserName = "baduser", Password = "badpass" };
+
+            //Arrange -create the controller 
+            AccountController target = new AccountController(mock.Object);
+
+            //Act-authenticate using valid credentials
+            ActionResult result = target.Login(model, "/MyURL");
+
+            //Assert 
+            Assert.IsInstanceOfType(result, typeof(ActionResult));
+            Assert.IsFalse(((ViewResult)result).ViewData.ModelState.IsValid);
+        }
+    }
+}
---
+++ SportsStore.UnitTests/ImageTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SportsStore.Domain.Entities;
+using Moq;
+using SportsStore.Domain.Abstract;
+using System.Linq;
+using System.Web.Mvc;
+using SportsStore.WebUI.Controllers;
+
+namespace SportsStore.UnitTests
+{
+    [TestClass]
+    public class ImageTests
+    {
+        [TestMethod]
+        public void Can_Retrieve_Image_Data() 
+        {
+            //Arange -create a product with image data
+            Product prod = new Product
+            {
+                ProductID = 2,
+                Name = "Test",
+                ImageData = new byte[] { },
+                ImageMimeType = "image/png"
+            };
+
+            // Arrange - create the mock repository
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] { new Product { ProductID = 1, Name = "P1" }, prod,
+                new Product { ProductID = 2, Name = "P2" } }.AsQueryable());
+
+            // Arrange - create the controller
+            ProductController target = new ProductController(mock.Object);
+
+            // Act - call the GetImage action method
+            ActionResult result = target.GetImage(2);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(FileResult));
+            Assert.AreEqual(prod.ImageMimeType, ((FileResult)result).ContentType);
+
+        }
+
+        [TestMethod]
+        public void Cannot_Retrieve_Image_Data_For_Invalid_ID()
+        {
+            // Arrange - create the mock repository
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] {
+            new Product {ProductID = 1, Name = "P1"},
+            new Product {ProductID = 2, Name = "P2"}
+            }.AsQueryable());
+            // Arrange - create the controller
+            ProductController target = new ProductController(mock.Object);
+            // Act - call the GetImage action method
+            ActionResult result = target.GetImage(100);
+            // Assert
+            Assert.IsNull(result);
+        }
+    }
+}
